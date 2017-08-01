5a4d3e7687d6aa3b42f96f71bebbd97ff2a34856
Victor Dozal <v.dozal@live.com>
2017-08-01 07:22:34 +0000

    Removed trailing spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, through: :user_groups
-  
+
   attr_accessor :role
-  
-  validates :first_name, presence: true, 
+
+  validates :first_name, presence: true,
                  length: { maximum: 20 }
   validates :last_name, presence: true,
                  length: { maximum: 20 }
-                 
+
  def role(group_id)
     user_group = user_groups.find_by(group_id: group_id)
-    
+
     return user_group.role
   end
-    
+
   def is_organizer?(group_id)
     # refactor right hand side string into a constant
     role(group_id) == Role.find_by(name: "organizer")
   end
-    
+
   def full_name
     return first_name + ' ' + last_name
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(first_name: "Joe", 
+    @user = User.new(first_name: "Joe",
                      last_name: "User")
   end
-  
+
   def add_mock_data(role_name)
     @user.save
     @role = Role.find_by(name: role_name)
@@ -16,7 +16,7 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.first_name = "     "
     @user.last_name = "     "
@@ -28,26 +28,26 @@ class UserTest < ActiveSupport::TestCase
     @user.last_name = "b" * 50
     assert_not @user.valid?
   end
-  
+
   test "#role returns the correct role" do
     add_mock_data("organizer")
     role = @user.role(@group.id)
-    
+
     assert role == @role
   end
-  
+
   test "#is_organizer? returns true if user is organizer" do
     add_mock_data("organizer")
-    
+
     assert @user.is_organizer?(@group.id)
   end
-  
+
   test "#is_organizer? returns false if user is not organizer" do
     add_mock_data("presenter")
-    
+
     assert_not @user.is_organizer?(@group.id)
   end
-  
+
   test "#full_name returns the user's full name" do
     full_name = @user.first_name + " " + @user.last_name
     assert @user.full_name == full_name
