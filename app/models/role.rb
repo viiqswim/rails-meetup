25d412fe9973685397c386ea0d84291dfc38f637
Victor Dozal <v.dozal@live.com>
2017-08-01 05:43:30 +0000

    Added Role validation and tests

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,3 @@
 class Role < ApplicationRecord
+    validates :name, presence: true
 end
--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @role = Role.new(name: "Organizer")
+  end
+  
+  test "should be valid" do
+    assert @role.valid?
+  end
+  
+  test "name should be present" do
+    @role.name = "     "
+    assert_not @role.valid?
+  end
 end
