f160f18a592f4d920f997255cd16de3367606bbd
Victor Dozal <v.dozal@live.com>
2017-08-01 05:43:55 +0000

    Added UserGroup validation and tests

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,4 +2,8 @@ class UserGroup < ApplicationRecord
   belongs_to :user
   belongs_to :group
   belongs_to :role
+    
+  validates :user_id, presence: true
+  validates :group_id, presence: true
+  validates :role_id, presence: true
 end
--- test/models/user_group_test.rb
+++ test/models/user_group_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    group = Group.create(name: "test-group")
+    user = User.create(first_name: "Joe", last_name: "User")
+    role = Role.first
+    @user_group = UserGroup.new(user_id: user.id, group_id: group.id, role_id: role.id)
+  end
+  
+  test "should be valid" do
+    assert @user_group.valid?
+  end
+  
+  test "should not be valid" do
+    @user_group.user_id = nil
+    @user_group.group_id = nil
+    @user_group.role_id = nil
+    assert_not @user_group.valid?
+  end
 end
