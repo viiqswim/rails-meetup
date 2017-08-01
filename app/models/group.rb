cbf72e6d67d701e49b0cbd8f482b033ee8f291e8
Victor Dozal <v.dozal@live.com>
2017-08-01 06:33:42 +0000

    Added 'add_member' method to Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
     
   validates :name, presence: true
+  
+  def add_member(user, role)
+    return UserGroup.create(group_id: id, user_id: user.id, role_id: role.id)
+  end
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,16 +1,32 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-    def setup
-        @group = Group.new(name: "My First Meetup")
-    end
-    
-    test "should be valid" do
-        assert @group.valid?
-    end
+  def setup
+    @group = Group.new(name: "My First Meetup")
+  end
   
-    test "name should be present" do
-        @group.name = "     "
-        assert_not @group.valid?
-    end
+  def add_mock_members
+    @group.save
+    user1 = User.create(first_name: "Joe", last_name: "User")
+    user2 = User.create(first_name: "Joe", last_name: "User")
+    role1 = Role.create(name: "organizer")
+    role2 = Role.create(name: "participant")
+    @group.add_member(user1, role1)
+    @group.add_member(user2, role2)
+  end
+  
+  test "should be valid" do
+    assert @group.valid?
+  end
+  
+  test "name should be present" do
+    @group.name = "     "
+      assert_not @group.valid?
+  end
+  
+  test "#add_member adds a memer" do
+    add_mock_members
+    
+    assert @group.users.count == 2
+  end
 end
